bc6400056dd01051d6b3aa6c1ed629d2bb0df3f6
Alejandra Gonzalez <blyxyas@gmail.com>
2026-08-20 11:46:19 +0200

    Ignore a bunch of benchmarks

--- site/frontend/build_clippy.sh
+++ site/frontend/build_clippy.sh
@@ -12,6 +12,20 @@ cargo build --profile clippy-build
 export LD_LIBRARY_PATH="$(rustc --print=sysroot)/lib"
 cd ../../../ # Back to project root
 
+# These ones depend on libc, which we cannot compile for some reason
+EXTRA_ARGS="--exclude cargo-0.87.1 \
+--exclude eza-0.21.2 \
+--exclude html5ever-0.31.0 \
+--exclude html5ever-0.31.0-new-solver \
+--exclude image-0.25.6 \
+--exclude libc-0.2.172 \
+--exclude nalgebra-0.33.0 \
+--exclude nalgebra-0.33.0-new-solver \
+--exclude projection-caching \
+--exclude ripgrep-14.1.1 \
+--exclude ripgrep-14.1.1-nll \
+--exclude ripgrep-14.1.1-tiny"
+
 # cargo r --bin collector -r profile_local callgrind +nightly-2026-08-14 --profiles Clippy,Check --clippy site/frontend/rust-clippy/target/clippy-build/clippy-driver
 # cargo r --bin collector -r bench_local +nightly-2026- --profiles Clippy,Check --clippy site/frontend/rust-clippy/target/clippy-build/clippy-driver
 
